ee82e1896f38caa133886f6319776a8a181e7a2e
mcbauman <m.c.baumann@icloud.com>
2022-08-11 10:53:56 +0200

    Put Request realized

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -4,10 +4,12 @@ import { BiEditAlt, BiAddToQueue } from "react-icons/bi"
 import {MdDownloadDone} from "react-icons/md"
 import {AiOutlineCloseCircle} from "react-icons/ai"
 import {AiOutlineDelete}from "react-icons/ai"
+import { GiConfirmed } from "react-icons/gi" 
 
 export default function ToDoList(props){
     const [items,setItems]=useState([])
     const [show, setShow]=useState(false)
+    const [show2, setShow2]=useState(false)
     const [ItemName, setTaksName]=useState()
     const [Discription, setTaskDesc]=useState()
 
@@ -45,6 +47,15 @@ export default function ToDoList(props){
         .catch(err=>console.log(err))
     }
 
+    function updateItem(id, itemName,discription){
+        axios.put("https://localhost:7122/Item",{UserId:props.user,id,itemName,discription})
+        .then(res=>{
+            console.log("UPDATE RESPONSE",res);
+            GetItems();
+        })
+        .catch(err=>console.log(err))
+    }
+
     useEffect(()=>{
         GetItems();
     },[])
@@ -62,8 +73,16 @@ export default function ToDoList(props){
                             <div>{item.id}</div>
                             <div>{item.itemName}</div>
                             <div>{item.discription}</div>
-                            <button ><BiEditAlt/></button>
+                            {/* <button ><BiEditAlt/></button> */}
                             <button onClick={()=>removeItem(item.id,item.itemName,item.discription)}><AiOutlineDelete/></button>
+                            {show2==item.id?(
+                                <form onSubmit={(e)=>{updateItem(item.id,item.itemName,item.discription); e.preventDefault()}}>
+                                    <input type="text" placeholder={item.itemName}/>
+                                    <input type="text" placeholder={item.discription}/>
+                                    <button type="submit"><GiConfirmed/></button>
+                                    <button onClick={()=>setShow2(false)}><AiOutlineCloseCircle/></button>
+                                </form>
+                            ):<button onClick={()=>setShow2(item.id)}><BiEditAlt/></button>}
                         </section>
                 ))
                 ):<p>Loading ...</p>}
